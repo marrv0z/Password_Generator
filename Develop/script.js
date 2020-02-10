3c5ae226520969ea02026d930d609adb44d91400
marrv0z <marrv00@outlook.com>
2020-02-09 22:25:38 -0600

    added if statement

--- Develop/script.js
+++ Develop/script.js
@@ -179,8 +179,20 @@ function generatePassword(){
     while (passwordWill.length !== howLong){
       passwordWill = passwordWill + lowerLetter[abcGen()];
     }
+
+    
   } 
-  
+
+  else if (lowerCheck === true && upperCheck === false && specCharCheck === true && numberCheck === false) {
+    lengthEach = howLong/3;
+    lengthEach = Math.floor(lengthEach);
+    for (var i=0; i< lengthEach; i++){
+      passwordWill = passwordWill + lowerLetter[abcGen()] + specChar[numCharGen()];
+    }
+    while (passwordWill.length !== howLong){
+      passwordWill = passwordWill + lowerLetter[abcGen()];
+    }
+  }
   // in case they choose all false, the password generator will not work, and the user will have to click the button again
   else{
     alert("Sorry, your choices do not qualify to generate a password, please try again.");
